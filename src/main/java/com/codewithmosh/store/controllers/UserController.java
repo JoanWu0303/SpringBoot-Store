650cbfb9d82a7ecced78f798c45969f3341b4a83
JoanWu <joanwu11226@gmail.com>
2025-06-08 22:30:33 -0400

    extracting query parameters

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,19 +1,14 @@
 package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.UserDto;
-import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
-import lombok.extern.java.Log;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Set;
 
 @AllArgsConstructor
 @RestController
@@ -22,8 +17,14 @@ public class UserController {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
-    public Iterable<UserDto> getAllUsers(){
-        return userRepository.findAll()
+    @GetMapping
+    public Iterable<UserDto> getAllUsers(@RequestParam(required = false, defaultValue = "", name = "sort") String sortBy){
+
+        if(!Set.of("name", "email").contains(sortBy)){ //valid sort name
+            sortBy = "name";
+        }
+
+        return userRepository.findAll(Sort.by(sortBy))
                 .stream()
                 .map(userMapper::toDto)
                 .toList();
