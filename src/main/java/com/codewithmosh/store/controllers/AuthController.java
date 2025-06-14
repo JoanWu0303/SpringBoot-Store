d3d5c5cb3e42fdd3cab342f7d4e0b667cc887305
JoanWu <joanwu11226@gmail.com>
2025-06-13 22:46:21 -0400

    access the current user

--- src/main/java/com/codewithmosh/store/controllers/AuthController.java
+++ src/main/java/com/codewithmosh/store/controllers/AuthController.java
@@ -1,7 +1,10 @@
 package com.codewithmosh.store.controllers;
 
 import com.codewithmosh.store.dtos.JwtResponse;
+import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.dtos.LoginRequest;
+import com.codewithmosh.store.mappers.UserMapper;
+import com.codewithmosh.store.repositories.UserRepository;
 import com.codewithmosh.store.services.JwtService;
 import lombok.AllArgsConstructor;
 import jakarta.validation.Valid;
@@ -10,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +24,8 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest request) {
@@ -28,6 +34,23 @@ public class AuthController {
         );
         var token = jwtService.generateToken(request.getEmail());
         return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(){
+        //get the principal
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var email = (String) authentication.getPrincipal();
+
+        //find user by email
+        var user = userRepository.findByEmail(email).orElse(null);
+        if(user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        //map the user to dto
+        var userDto = userMapper.toDto(user);
+        return ResponseEntity.ok(userDto);
     }
 
     @PostMapping("/validate")
