adc19815f0e45c3633a23170697e5afc3364154c
JoanWu <joanwu11226@gmail.com>
2025-06-13 21:33:51 -0400

    managing the secrets key
    generate a random key using "openssl rand -base64 64"

--- src/main/java/com/codewithmosh/store/services/JwtService.java
+++ src/main/java/com/codewithmosh/store/services/JwtService.java
@@ -4,12 +4,16 @@ import com.codewithmosh.store.entities.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
     public String generateToken(String email) {
         final long tokenExpiration = 86400; //1 day
         return Jwts
@@ -17,7 +21,7 @@ public class JwtService {
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-                .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact(); //generate the token
     }
 }
