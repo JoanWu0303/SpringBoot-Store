5ce8c4806cb5281039669ec23ab831b206f2f441
JoanWu <joanwu11226@gmail.com>
2025-06-13 20:13:53 -0400

    building the login API

--- src/main/java/com/codewithmosh/store/repositories/UserRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/UserRepository.java
@@ -6,6 +6,12 @@ import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
