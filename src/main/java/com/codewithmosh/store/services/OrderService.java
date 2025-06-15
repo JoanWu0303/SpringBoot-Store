c4c74e42b7abb8de830f3f3b0da1d9a23acb9480
JoanWu <joanwu11226@gmail.com>
2025-06-15 15:53:12 -0400

    getting a single order using orderId

--- src/main/java/com/codewithmosh/store/services/OrderService.java
+++ src/main/java/com/codewithmosh/store/services/OrderService.java
@@ -1,9 +1,11 @@
 package com.codewithmosh.store.services;
 
 import com.codewithmosh.store.dtos.OrderDto;
+import com.codewithmosh.store.exceptions.OrderNotFoundException;
 import com.codewithmosh.store.mappers.OrderMapper;
 import com.codewithmosh.store.repositories.OrderRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,8 +23,22 @@ public class OrderService {
         var user = authService.getCurrentUser();
 
         //get the orders that created by this user
-        var orders = orderRepository.getAllByCustomer(user);
+        var orders = orderRepository.getOrdersByCustomer(user);
 
         return orders.stream().map(orderMapper::toDto).toList();
+    }
+
+    public OrderDto getOrder(Long orderId) {
+        //check if the order exist
+        var order = orderRepository.getOrderWithItems(orderId).orElseThrow(OrderNotFoundException::new);
+
+        //check if the order belongs to this user
+        var user = authService.getCurrentUser();
+        if(!order.isPlacedBy(user)) {
+            throw new AccessDeniedException("You do not have permission to access this order");
+        }
+
+        //return order and 200
+        return orderMapper.toDto(order);
     }
 }
